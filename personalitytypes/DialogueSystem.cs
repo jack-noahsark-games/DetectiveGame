1cb06cf2f29378cbfe3e848c1e2d133e68f186e5
jack-noahsark-games <jack123atkinson@outlook.com>
2025-12-01 22:45:51 +0000

    implemented DialogueSystem class

--- personalitytypes/DialogueSystem.cs
+++ personalitytypes/DialogueSystem.cs
@@ -13,16 +13,40 @@ namespace DetectiveGame
         private int dialogueIndex;
         private string currentTopic;
 
-        public bool IsTopicActive => currentLines != null;
 
-        public void StartTopic(Person npc, string topic)
+        public void StartTopic(Person npc, string topic) //load the values 
         {
             this.npc = npc;
             this.currentTopic = topic;
 
             currentLines = npc.GetDialogueForTopic(topic);
+
             dialogueIndex = 0;
         }
 
+        public bool HasNextLine()
+        {
+            if (currentLines == null)
+                return false;
+
+            return dialogueIndex < currentLines.Count; //if dialogue index less than count of total lines, return true. dynamic true/false return type here
+        }
+
+        public string GetNextLine()
+        {
+            if (!HasNextLine())
+                    return null;
+            string line = currentLines[dialogueIndex];
+            dialogueIndex++;
+            return line;
+        }
+
+        public void EndTopic() //remove any leftover references to the variables, i.e. wipe the stored npc value, wipe currentLines
+        {
+            npc = null;
+            currentLines = null;
+            currentTopic = null;
+            dialogueIndex = 0;
+        }
     }
 }
