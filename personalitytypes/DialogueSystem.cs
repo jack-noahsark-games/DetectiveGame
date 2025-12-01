847f9d61cbc8082c64ef4fe02f5325d1b0cf07f6
jack-noahsark-games <jack123atkinson@outlook.com>
2025-12-01 22:22:40 +0000

    initial push

---
+++ personalitytypes/DialogueSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//initial push
+namespace DetectiveGame
+{
+    public class DialogueSystem
+    {
+        private Person npc;
+        private List<string> currentLines;
+        private int dialogueIndex;
+        private string currentTopic;
+
+        public bool IsTopicActive => currentLines != null;
+
+        public void StartTopic(Person npc, string topic)
+        {
+            this.npc = npc;
+            this.currentTopic = topic;
+
+            currentLines = npc.GetDialogueForTopic(topic);
+            dialogueIndex = 0;
+        }
+
+    }
+}
