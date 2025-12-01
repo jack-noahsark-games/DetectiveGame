f7b12b06399dad915c47412ad96b4a610c0dcdbf
jack-noahsark-games <jack123atkinson@outlook.com>
2025-12-01 23:41:18 +0000

    Implemented EvidenceSystem, calls AddEvidence and now the TryUnlockEvidence method returns the Evidence, not strictly necessary now but can be used in future.

---
+++ personalitytypes/EvidenceSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectiveGame
+{
+    public class EvidenceSystem
+    {
+
+        public Evidence TryUnlockEvidence(Person npc, string currentTopic, Case activeCase)
+        {
+            int mood = npc.Mood;
+            Evidence ev = null;
+
+            if (currentTopic == "alibi" && mood >= 60)
+            {
+                ev = activeCase.GetEvidenceById("bar_receipt"); //pass ID into GetEvidenceById
+
+            }
+            else if (currentTopic == "night" && mood >= 55)
+            {
+                ev = activeCase.GetEvidenceById("shouting_in_alley");  //pass ID into GetEvidenceById
+            }
+            else if (currentTopic == "victim" && mood >= 50)
+            {
+                ev = activeCase.GetEvidenceById("victim_unknown_man");  //pass ID into GetEvidenceById
+            }
+
+            if (ev != null)
+            {
+                activeCase.AddEvidence(ev);
+            }
+
+            return ev;
+        }
+    }
+}
